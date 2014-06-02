cd99b2e37a88d4c8d196638564d29c0b70746687
sama <andreysama@gmail.com>
2014-06-01 23:30:37 -0600

    mi parte del proyecto -sama-

---
+++ ProyectoC++/compara.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   compara.h
+ * Author: sama
+ *
+ * Created on June 1, 2014, 8:52 PM
+ */
+
+#ifndef COMPARA_H
+#define	COMPARA_H
+
+class compara {
+public:
+    compara();
+    compara(const compara& orig);
+    virtual ~compara();
+    void sacapromedios();
+    void arreglo_prom();
+    
+private:
+
+};
+
+#endif	/* COMPARA_H */
+
